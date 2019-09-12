92ea34d29ca4e59ac18ed0653a4b46d2544045ca
kyriosdata <fabio@inf.ufg.br>
2019-09-12 08:44:18 -0300

    semantica pode ser melhor

--- singleton/src/main/java/com/github/kyriosdata/exemplo/Tarifa.java
+++ singleton/src/main/java/com/github/kyriosdata/exemplo/Tarifa.java
@@ -1,5 +1,23 @@
 package com.github.kyriosdata.exemplo;
 
+/**
+ * Entendo que Tarifa não é um uso "recomendado", afinal, temos muitas
+ * tarifas, e não apenas uma. De fato, seria desejável termos apenas uma
+ * única instância de Tarifa para refletir o que ocorre no mundo real,
+ * contudo, temos um cenário bem diferente deste.
+ *
+ * Adicionalmente, em vez de Tarifa, podemos ter a classe TabelaTarifas, na
+ * qual o processo de criação de uma instância é oneroso e, não apenas por
+ * este motivo, mas também pelo fato de que as atualizações seriam frequentes
+ * ao longo de um dia e, neste caso, não faz sentido repetir o mesmo processo
+ * onde teríamos várias instâncias de TabelaTarifas.
+ *
+ * Neste contexto, melhor seria termos uma única instância e todos saberiam
+ * que, se houve uma atualização, então ela é compartilhada por todos.
+ *
+ * Finalmente, o exemplo mostra como implementar o Singleton, mas não o faz
+ * com uma semântica adequada, diria que é um exemplo sintático apenas. 
+ */
 public class Tarifa {
     private static double tarifa = 1.5;
     private static double tarifaBaixaRenda = 1.2;
