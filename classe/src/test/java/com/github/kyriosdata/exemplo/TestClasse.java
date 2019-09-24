176faf429808699e3854e8bd33c2122ae2257de6
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2019-09-24 09:13:22 -0300

    Sutil

--- classe/src/test/java/com/github/kyriosdata/exemplo/TestClasse.java
+++ classe/src/test/java/com/github/kyriosdata/exemplo/TestClasse.java
@@ -9,6 +9,7 @@ class TestClasse {
     @Test
     void verificaCloseChamado() {
         assertThrows(RuntimeException.class, () -> {
+            // Parece que está funcionando, mas o null provoca exceção que não é aquela do seu método close!!!!!
             try (ClasseCriada obj = new ClasseCriada(null)) {
             }
         });
